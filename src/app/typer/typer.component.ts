86a70c222f55b7e79d671fc992c1a31e230e2670
Saikrishna <saikrishna.p@iorta.in>
2024-09-04 12:30:46 +0530

    feat(typer): Implemented the typing capability and the text will be scrollable

--- src/app/typer/typer.component.ts
+++ src/app/typer/typer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'typer',
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./typer.component.css']
 })
 export class TyperComponent {
-  text: string = 'Hello world!';
+  // text: string = 'Hello world!';
+  text: string = 'apple banana orange grape lemon lime peach pear mango cherry plum apricot kiwi melon berry fig date papaya pomegranate coconut watermelon strawberry blueberry raspberry blackberry toast bread butter cheese milk cream coffee tea juice soda water soup salad rice pasta noodles burger pizza taco steak chicken fish egg bacon ham sausage hotdog sandwich cookie cake pie donut muffin candy chocolate ice cream yogurt cereal oats pancake waffle syrup honey jam jelly nut almond walnut cashew pecan sesame poppy sugar salt pepper garlic onion ginger basil thyme mint parsley rosemary dill chili curry sauce ketchup mustard mayo vinegar olive canola sunflower butter margarine tart brownie cupcake parfait fruit vegetable meat poultry seafood crab lobster scallop mussel shrimp clams oysters tuna trout salmon cod haddock snapper mackerel sardine anchovy anchovy herring squid octopus eel mussel oyster clam crawfish lobster crab shrimp fish anchovy mackerel trout salmon tuna cod haddock snapper sardine clam mussel squid octopus eel eel anchovy squid octopus mussel clam oyster shrimp lobster crab';
   inputText: string = '';
   textArray: string[] = [];
 
@@ -21,8 +22,15 @@ export class TyperComponent {
     return this.inputText[index] === this.textArray[index] ? 'correct' : 'incorrect';
   }
 
-  onInputChange() {
-    // Additional logic can be added here if needed
+  @HostListener('document:keydown', ['$event'])
+  onKeydown(event: KeyboardEvent) {
+    const key = event.key;
+
+    if (key === 'Backspace') {
+      this.inputText = this.inputText.slice(0, -1);
+    } else if (key.length === 1) { // Only process printable characters
+      this.inputText += key;
+    }
   }
 
   isSpace(char: string): boolean {
