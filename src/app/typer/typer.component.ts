a9e076e276be855e44b5e08e807cdd4b59e7ce44
Saikrishna <saikrishna.p@iorta.in>
2024-09-05 01:20:48 +0530

    feat(typer): Added reset and timing options

--- src/app/typer/typer.component.ts
+++ src/app/typer/typer.component.ts
@@ -7,8 +7,8 @@ import { Component, HostListener} from '@angular/core';
 })
 export class TyperComponent {
   // text: string = 'Hello world!';
-  // text: string = `the and is in on at by with for from to of a an as be do have it if he she they we you I me him her them us our your my mine this that these those what which who whom where when why`;
-  text: string = `the and is in on`;
+  text: string = `the and is in on at by with for from to of a an as be do have it if he she they we you I me him her them us our your my mine this that these those what which who whom where when why`;
+  // text: string = `the and is in on`;
   // text: string = `The quick brown fox jumped over the lazy dogs.`;
   lines: string[] = [];
   currentLineIndex: number = 0;
@@ -18,32 +18,43 @@ export class TyperComponent {
   correctKeystroke : number = 0;
   wrongKeystroke : number = 0;
   index : any = 0;
+  grossWPM : any ;
+  netWPM : any ;
+  accuracy : any;
+  showPopUp : boolean = false;
+  timerStarted : boolean = false;
+  typingStarted : boolean = false;
+  timingData : any= 1;
 
   constructor() {
     this.lines = this.text.split(' '); // Split text into words for display
   }
   ngOnInit (){
-    setTimeout(()=>{
-      const inputCharacters = this.allKeystrokes.map(item => item.keyValue).join('');
-      let correctCount = 0;
-let incorrectCount = 0;
+//     setTimeout(()=>{
+//       const inputCharacters = this.allKeystrokes.map(item => item.keyValue).join('');
+//       let correctCount = 0;
+// let incorrectCount = 0;
 
-// Compare characters
-for (let i = 0; i < inputCharacters.length; i++) {
-    if (inputCharacters[i] === this.text[i]) {
-        correctCount++;
-    } else {
-        incorrectCount++;
-    }
-}
-// calculating the Gross wpm : 
-let grossWpm = this.totalKeystroke/ 5;
-let netWpm = correctCount / 5;
-let accuracy = (netWpm * 100) / grossWpm;
+// // Compare characters
+// for (let i = 0; i < inputCharacters.length; i++) {
+//     if (inputCharacters[i] === this.text[i]) {
+//         correctCount++;
+//     } else {
+//         incorrectCount++;
+//     }
+// }
+// // calculating the Gross wpm : 
+// let grossWpm = this.totalKeystroke/ 5;
+// let netWpm = correctCount / 5;
+// let accuracy = (netWpm * 100) / grossWpm;
+// this.grossWPM = grossWpm;
+// this.netWPM = netWpm;
+// this.accuracy = accuracy;
+// this.showPopUp = true;
 
-      alert("Times up. \n Gross WPM : "+ grossWpm + " \n Net WPM : "+netWpm+" \n Accuracy : "+accuracy)
-      console.log("Times up. TotalKeystroke : "+ this.totalKeystroke + " Correct keystroke : " + correctCount + " Wrong keystroke : "+ (this.wrongKeystroke + incorrectCount)  +" and the Gross WPM is this : " + grossWpm, " and the netWpm is this : ", netWpm, " and the accuracy is this : ", accuracy )
-    }, 10000)
+//       // alert("Times up. \n Gross WPM : "+ grossWpm + " \n Net WPM : "+netWpm+" \n Accuracy : "+accuracy)
+//       // console.log("Times up. TotalKeystroke : "+ this.totalKeystroke + " Correct keystroke : " + correctCount + " Wrong keystroke : "+ (this.wrongKeystroke + incorrectCount)  +" and the Gross WPM is this : " + grossWpm, " and the netWpm is this : ", netWpm, " and the accuracy is this : ", accuracy )
+//     }, 10000)
   }
 
   getDisplayedLine(): string[] {
@@ -75,8 +86,10 @@ let accuracy = (netWpm * 100) / grossWpm;
     const key = event.key;
     if ( /^[a-zA-Z0-9 ]$/.test(key) ) {
       this.totalKeystroke++;
+      if ( this.typingStarted == false ) {
+        this.startTimer(this.timingData);
+      }
     }
-    // console.log("The key is this ", key, " and the total keystroke is this ", this.totalKeystroke, " and the wrong keyStroke is this : ", this.wrongKeystroke , " and the index is this : ", this.totalKeystroke -1)
     if (key === 'Backspace') {
       // this.wrongKeystroke++;
       this.allKeystrokes.pop()
@@ -93,8 +106,6 @@ let accuracy = (netWpm * 100) / grossWpm;
       }
       this.inputText += key;
     }
-    // console.log("The allKeystroke is this : ", this.allKeystrokes)
-    // this.inputText[index] === this.getDisplayedLine()[index] ? 'correct' : 'incorrect'
     let inputText = this.inputText.length;
     let displayedLine = this.getDisplayedLine();
     // Move to the next line if inputText matches or exceeds the length of the displayed line
@@ -106,5 +117,52 @@ let accuracy = (netWpm * 100) / grossWpm;
 
   isSpace(char: string): boolean {
     return char === ' ';
+  }
+
+  closePopup () : void {
+    this.showPopUp = false;
+  }
+
+  startTimer(time: any) : void {
+    setTimeout(()=>{
+      const inputCharacters = this.allKeystrokes.map(item => item.keyValue).join('');
+      let correctCount = 0;
+let incorrectCount = 0;
+
+// Compare characters
+for (let i = 0; i < inputCharacters.length; i++) {
+    if (inputCharacters[i] === this.text[i]) {
+        correctCount++;
+    } else {
+        incorrectCount++;
+    }
+}
+// calculating the Gross wpm : 
+let grossWpm = this.totalKeystroke/ 5;
+let netWpm = correctCount / 5;
+let accuracy = (netWpm * 100) / grossWpm;
+this.grossWPM = grossWpm;
+this.netWPM = netWpm;
+this.accuracy = accuracy;
+this.showPopUp = true;
+    }, time*1000)
+  }
+
+  resetTyping(): void {
+    this.currentLineIndex = 0;
+    this.inputText = '';
+    // this.correctKeyStroke = 0;
+    this.correctKeystroke = 0;
+    this.wrongKeystroke = 0;
+    this.timerStarted = false;
+    // window.location.reload();
+    this.getDisplayedLine(); // Reset the displayed line
+    this.getNextLine(); // Reset the next line preview
+    this.typingStarted = false
+    // this.startTimer(30); // Restart the 30-second timer
+  }
+
+  setTyping (input: any) {
+    this.timingData = input;
   }
 }
